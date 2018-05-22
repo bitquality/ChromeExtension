fe046df36683d893f018822f2c83ce5f16afeb96
Gopi Krishna Mullapudi <gopi.mullapudi@outlook.com>
2018-05-22 10:04:00 -0400

    Update csdom.js

--- csdom.js
+++ csdom.js
@@ -5,28 +5,31 @@ document.addEventListener("click", function (event) {
 
 // The below code doesn't work 
 function listenToClicks(targetDocument) {
-    console.log('cs: listenToClicks function is called');
+    console.log('cs: listenToClicks function  is called');
     targetDocument.addEventListener("click", function (event) {
-        alert('listenToClicks function: click event is fired');
-});
+        alert('listenToClicks  function: click event is fired');
+    });
 }
 
 // even The below code doesn't work 
 function stopListenToClicks() {
     document.removeEventListener("click", function (event) {
         alert('stopListenToClicks click is fired');
-});
+    });
 }
 
 chrome.runtime.onMessage.addListener(function (request, sender, sendResponse) {
-    console.log("cs: onMessage addListener is fired ",request);
-    if (request.subject == "listen"){
+    console.log("cs: onMessage addListener is fired ", request);
+
+    if (request.subject == "listen") {
         listenToClicks(document);
-}
-    elseif(request.subject == "stoplisten")
+    }
+    else if (request.subject == "stoplisten")
         stopListenToClicks();
+        
     sendResponse({ msg: "cs: click events are now fired from listentoclicks event is function" });
     return true;
 });
 
 console.log("cs script loaded");
+
