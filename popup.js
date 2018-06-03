c821b50b9a14c5206adbc4e6cb73663437d7dd1e
Gopi Krishna Mullapudi <gopi.mullapudi@outlook.com>
2018-06-03 08:31:06 -0400

    Update popup.js

--- popup.js
+++ popup.js
@@ -1,10 +1,11 @@
 // Once the DOM is ready...
 window.addEventListener('DOMContentLoaded', function () {
   // ...query for the active tab...
-  console.log("cs: DOMContentLoaded fired");
-  alert("cs: DOMContentLoaded");
+  //console.log("cs: DOMContentLoaded fired");
+  //alert("cs: DOMContentLoaded");
   document.getElementById("clickMe").addEventListener("click", clickMeFunc);
 
+  document.getElementById("stopMe").addEventListener("click", stopMeFunc);
 });
 
 console.log("popup loaded");
@@ -15,6 +16,18 @@ function clickMeFunc() {
 
   chrome.runtime.sendMessage(
     { 'from': 'popup', 'subject': 'listen' },
+    function (response) {
+      console.log(response);
+    }
+  );
+}
+
+function stopMeFunc() {
+  console.log('stopMeFunc is fired');
+  //alert('clickMe is fired');
+
+  chrome.runtime.sendMessage(
+    { 'from': 'popup', 'subject': 'stoplisten' },
     function (response) {
       console.log(response);
     }
