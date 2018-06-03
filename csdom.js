08d31f341369d6970b56babeeac7d8c6cee437fd
Gopi Krishna Mullapudi <gopi.mullapudi@outlook.com>
2018-06-03 08:30:43 -0400

    Update csdom.js

--- csdom.js
+++ csdom.js
@@ -1,35 +1,60 @@
-//this code always works
-document.addEventListener("click", function (event) {
-    alert('i always work.clicked');
-});
 
 // The below code doesn't work 
-function listenToClicks(targetDocument) {
+function listenToClicks(targetDoc) {
     console.log('cs: listenToClicks function  is called');
-    targetDocument.addEventListener("click", function (event) {
-        alert('listenToClicks  function: click event is fired');
-    });
+    targetDoc.addEventListener("click", clickListener,false);
+}
+
+var clickListener = function(e){
+    alert('cs: listenToClicks function onclick event event  is fired',e);
+    e = e || window.event;
+    var target = e.target || e.srcElement,        text = target.textContent || (text!=undefined ? text.innerText:undefined);   
+    console.log('cs: clickListener ',target);
 }
 
 // even The below code doesn't work 
 function stopListenToClicks() {
-    document.removeEventListener("click", function (event) {
-        alert('stopListenToClicks click is fired');
-    });
+    // document.removeEventListener("click", function (event) {
+    //     alert('stopListenToClicks click is fired');
+    // });
+
+    document.removeEventListener("click", clickListener,false);
+
 }
 
-chrome.runtime.onMessage.addListener(function (request, sender, sendResponse) {
-    console.log("cs: onMessage addListener is fired ", request);
 
-    if (request.subject == "listen") {
+
+var csdomPortListener = function (msg) {
+    console.log("csdomPortListener called  " + msg);
+    if (msg.subject == "listen") {
+        console.log("listen request received  " + msg);
+
         listenToClicks(document);
+        port.postMessage({ info: "csdomPort istenToClicks called " });
+
     }
-    else if (request.subject == "stoplisten")
+    else if (msg.subject == "stoplisten") {
+        console.log("stoplisten request received  " + msg);
+
         stopListenToClicks();
-        
-    sendResponse({ msg: "cs: click events are now fired from listentoclicks event is function" });
-    return true;
+    }
+};
+
+var port = chrome.runtime.connect({ name: "csdomPort" });
+port.onMessage.addListener(csdomPortListener);
+port.onDisconnect.addListener(function () {
+    //update background i am disconnected .
+    // or background will be auto notified if it has port.onconnect
 });
+port.postMessage({ info: "csdomPort initialized.You can start using it now" });
 
-console.log("cs script loaded");
 
+
+console.log("cs script loaded 13");
+
+
+
+//this code always works
+document.addEventListener("click", function (event) {
+    alert('i always work.clicked');
+});
